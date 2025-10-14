8664d7ea5afc76f6f13af42c634524f9c1612a54
cooi123 <calebooi@hotmail.com>
2025-10-15 00:12:57 +1100

    add missing convex module

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,13 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as projectCandidates from "../projectCandidates.js";
+import type * as projectMember from "../projectMember.js";
+import type * as projects from "../projects.js";
+import type * as schemas_projectCandidate_schema from "../schemas/projectCandidate_schema.js";
+import type * as schemas_project_schema from "../schemas/project_schema.js";
+import type * as schemas_projectmember_schema from "../schemas/projectmember_schema.js";
+import type * as schemas_user_schema from "../schemas/user_schema.js";
 import type * as users from "../users.js";
 
 /**
@@ -24,6 +31,13 @@ import type * as users from "../users.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  projectCandidates: typeof projectCandidates;
+  projectMember: typeof projectMember;
+  projects: typeof projects;
+  "schemas/projectCandidate_schema": typeof schemas_projectCandidate_schema;
+  "schemas/project_schema": typeof schemas_project_schema;
+  "schemas/projectmember_schema": typeof schemas_projectmember_schema;
+  "schemas/user_schema": typeof schemas_user_schema;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
